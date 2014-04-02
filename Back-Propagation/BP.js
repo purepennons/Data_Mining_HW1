65b84c46ce591d1c8531a948d9187d926372643b
purepennons <y80811@gmail.com>
2014-04-02 22:51:05 +0800

    20140402 - backup

--- Back-Propagation/BP.js
+++ Back-Propagation/BP.js
@@ -33,16 +33,16 @@ function BP(){
 	this.numOfInput = 0;  //輸入數目
 	this.numOfHidden = 0;  //隱藏層數目
 	this.numOfOutput = 0;  //輸出數目
-	this.w_xh = null;  //輸入至隱藏層的權重
-	this.w_hy = null;  //隱藏層至輸出的權重
+	this.w_xh = null;  //輸入至隱藏層的權重，bit0為basis
+	this.w_hy = null;  //隱藏層至輸出的權重，bit0為basis
 	this.dw_xh = null;  //輸入至隱藏層的權重變化量
 	this.dw_hy = null;  //隱藏層至輸出的權重變化量
-	this.theta_h = null;  //隱藏層的閥值
-	this.theta_y = null;  //輸出層的閥值
-	this.dtheta_h = null;  //隱藏層的閥值的變化量
-	this.dtheta_y = null;  //輸出層的閥值的變化量
-	this.x = null;  //輸入資料
-	this.h = null;  //暫存隱藏層
+	// this.theta_h = null;  //隱藏層的閥值
+	// this.theta_y = null;  //輸出層的閥值
+	// this.dtheta_h = null;  //隱藏層的閥值的變化量
+	// this.dtheta_y = null;  //輸出層的閥值的變化量
+	this.x = null;  //輸入資料，bit0為basis
+	this.h = null;  //暫存隱藏層，bit0為basis
 	this.y = null;  //暫存輸出結果
 	this.finalW = null;  //final weight array
 	this.finalWArray = null;
@@ -54,13 +54,13 @@ function BP(){
 		if(typeof(numOfInput)=='number' && typeof(numOfHidden)=='number' && typeof(numOfOutput)=='number'){
 			if(numOfInput >= 0 && numOfHidden >= 0 && numOfOutput >=0){
 				this.numOfInput = parseInt(numOfInput + 1); //加上基本的node
-				this.numOfHidden = parseInt(numOfHidden + 1); //加上基本的node
+				this.numOfHidden = parseInt(numOfHidden);
 				this.numOfOutput = parseInt(numOfOutput);
 
 				//initial arrays for nodes
-				this.x = createArray(this.numOfInput, 1.0);
-				this.h = createArray(this.numOfHidden, 1.0);
-				this.y = createArray(this.numOfOutput, 1.0);
+				this.x = createArray(this.numOfInput, -1.0);
+				this.h = createArray(this.numOfHidden, -1.0);
+				this.y = createArray(this.numOfOutput, -1.0);
 
 				//initial matrixs for weights
 				this.w_xh = createMatrix(this.numOfInput, this.numOfHidden, 1.0);
@@ -104,8 +104,33 @@ function BP(){
 /*** Back Propagation main functions ***/
 	
 	//更新輸入資料與計算隱藏層與輸出層結果
-	this.update = function(){  
+	this.update = function(inputData, tranFunction){  //inputData為每筆資料的輸入，陣列表示
+		if(inputData.length != this.numOfInput -1){
+			return false;
+		}
 
+		//將每筆input讀入x
+		for(var v=0;v<inputData.length;v++){
+			this.x[v+1] = inputData[v];
+		}
+
+		//計算輸入層至隱藏層的輸出 - this.h
+		for(var i=0;i<this.numOfHidden;i++){
+			var sum = 0.0;
+			for(var j=0;j<this.numOfInput;j++){
+				sum += this.x[j]*this.w_xh[j][i];
+			}
+			this.h[i] = tranF(tranFunction, sum);
+		}
+
+		//計算隱藏層至輸出層的輸出 - this.y
+		for(var i=0;i<this.numOfOutput;i++){
+			var sum = 0.0;
+			for(var j=0;j<this.numOfHidden;j++){
+				sum+= this.h[j]*this.w_hy[j][i];
+			}
+			this.y[i] = tranF(tranFunction, sum);
+		}
 	}
 
 	//反傳遞學習函數反求變化量與權重
