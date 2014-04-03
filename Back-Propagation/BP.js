a6a7261413afb561a8839de6ca86fc3633cc7fd5
purepennons <y80811@gmail.com>
2014-04-04 02:52:19 +0800

    when use tang fundtion, bp is work.

--- Back-Propagation/BP.js
+++ Back-Propagation/BP.js
@@ -41,9 +41,13 @@ function BP(){
 	// this.theta_y = null;  //輸出層的閥值
 	// this.dtheta_h = null;  //隱藏層的閥值的變化量
 	// this.dtheta_y = null;  //輸出層的閥值的變化量
+	this.m_xh = null;
+	this.m_hy = null;
 	this.x = null;  //輸入資料，bit0為basis
 	this.h = null;  //暫存隱藏層，bit0為basis
 	this.y = null;  //暫存輸出結果
+	this.deltasOfOutput = null;
+	this.deltasOfHidden = null;
 	this.finalW = null;  //final weight array
 	this.finalWArray = null;
 	this.learningRate = 0.01;  //testing rate
@@ -69,14 +73,16 @@ function BP(){
 				//give random nums for arrays and weights
 				for(var i=0;i<this.numOfInput;i++){
 					for(var h=0;h<this.numOfHidden;h++){
-						this.w_xh[i][h] = getRandomNum(-0.5, 0.5);
+						this.w_xh[i][h] = getRandomNum(-0.2, 0.2);
 					}
 				}
 				for(var h=0;h<this.numOfHidden;h++){
 					for(var o=0;o<this.numOfOutput;o++){
-						this.w_hy[h][o] = getRandomNum(-0.5, 0.5);
+						this.w_hy[h][o] = getRandomNum(-2.0, 2.0);
 					}
 				}
+				this.deltasOfHidden = createArray(this.numOfHidden, 0.0);
+				this.dw_hy = createMatrix(this.numOfHidden, this.numOfOutput, 0.0);						
 				return true;
 			}
 		}
@@ -120,7 +126,7 @@ function BP(){
 			for(var j=0;j<this.numOfInput;j++){
 				sum += this.x[j]*this.w_xh[j][i];
 			}
-			this.h[i] = tranF(tranFunction, sum);
+			this.h[i] = this.tranF(tranFunction, sum);
 		}
 
 		//計算隱藏層至輸出層的輸出 - this.y
@@ -129,17 +135,59 @@ function BP(){
 			for(var j=0;j<this.numOfHidden;j++){
 				sum+= this.h[j]*this.w_hy[j][i];
 			}
-			this.y[i] = tranF(tranFunction, sum);
+			this.y[i] = this.tranF(tranFunction, sum);
 		}
+		return this.y;
 	}
 
 	//反傳遞學習函數反求變化量與權重
-	this.backPropagation = function(){
+	this.backPropagation = function(yd, learningRate, moment, dTranFunction){  
+		//計算輸出層誤差
+		this.deltasOfOutput = createArray(this.numOfOutput, 0.0);
+		for(var i=0;i<this.numOfOutput;i++){
+			var error = yd[i] - this.y[i];
+			this.deltasOfOutput[i] = this.diffTranF(dTranFunction, this.y[i]) * error;
+		}
 
+		//計算隱藏層誤差		
+		for(var i=0;i<this.numOfHidden;i++){
+			var error = 0.0;
+			for(var j=0;j<this.numOfOutput;j++){
+				error += this.deltasOfOutput[j] * this.w_hy[i][j];
+			}
+			this.deltasOfHidden[i] = this.diffTranF(dTranFunction, this.h[i]) * error;
+		}
+
+		//調整w_hy權重
+		for(var i=0;i<this.numOfHidden;i++){
+			for(var j=0;j<this.numOfOutput;j++){
+				this.dw_hy[i][j] = learningRate * this.deltasOfOutput[j] * this.h[i];
+				this.w_hy[i][j] += this.dw_hy[i][j] + moment * this.dw_hy[i][j];
+			}
+		}
+
+		//調整w_xh權重
+		this.dw_xh = createMatrix(this.numOfInput, this.numOfHidden, 0.0);		
+		for(var i=0;i<this.numOfInput;i++){
+			for(var j=0;j<this.numOfHidden;j++){
+				this.dw_xh[i][j] = learningRate * this.deltasOfHidden[j] * this.x[i];
+				this.w_xh[i][j] += this.dw_xh[i][j] + moment * this.dw_xh[i][j];
+			}
+		}
 	}
 
 	//訓練主程式
-	this.train = function(){
+	this.train = function(testSample, maxTrainTimes, learningRate, moment){
+		for(var times=0;times<maxTrainTimes;times++){
+			for(var sample in testSample){
+				var partSample = testSample[sample];
+				var inputs = partSample[0];
+				var yds = partSample[1];
+				var outputs = this.update(inputs, 'tanh');
+				this.backPropagation(yds, learningRate, moment, 'dTanh');
+				log('y = ' +  outputs[0] + ' yd = ' + yds);
+			}
+		}
 
 	}
 
@@ -150,8 +198,16 @@ function BP(){
 
 }
 
+
+var pat = [
+  [[0,0], [0]],
+  [[0,1], [1]],
+  [[1,0], [1]],
+  [[1,1], [0]]
+];
 var bp = new BP();
-bp.init(2, 3, 4);
-log(bp.diffTranF('dSigmoid', 0.5));
+bp.init(2, 2, 1);
+bp.train(pat, 10000, 0.5, 0.1);
+log(pat);
 
 
